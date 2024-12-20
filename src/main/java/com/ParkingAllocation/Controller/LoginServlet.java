75a0445f63acfc1d0d0b5517ee496dddcba223cf
Mehul Jain <97732513+mehuljain55@users.noreply.github.com>
2024-12-20 12:26:59 +0530

    Feature update

--- src/main/java/com/ParkingAllocation/Controller/LoginServlet.java
+++ src/main/java/com/ParkingAllocation/Controller/LoginServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/user/login")
+@WebServlet("/user/validateCredential")
 public class UserController extends HttpServlet {
 
     private UserDaoImpl userDaoImpl;
